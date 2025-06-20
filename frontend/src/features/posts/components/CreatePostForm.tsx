d73c43a6be183ccd00b83915eabbf65b1f87b8ef
AfazPaul1 <afazpaul@gmail.com>
2025-06-20 23:42:09 +0530

    feat: react hook form - default values, integration with controlled inputs, validation/error messages,  fieldState, formState body: - Defined FormData type / form Schema to support form values. we call the useForm generic and explicitly pass type param FormData - Pass Default values for all fields via defaultValues option (part of config object) when calling useForm - we Nested destructure errors from the formState which contains the current state of form. errors is an object which contains all the fields's errors if they exist. - when we pass validations in the options to register for each validation instead of a boolean we can pass a message if that is everything it needs such as required. to minLength which requires a value we can pass an object with a value and a message. - Integration with Mui - this Textfield renders a native input so theres no issue here but in cases it doesnt like select checkbox do not expose a native input's ref or onChange we need to use a  Controller component which will take care of the registration process. like register returns props like onchange (uses control object internally) which connects to input automatically. but when we do the same to complex components the onchange doesnt reach them so rhf doesnt know how to control or monitor them. we give controller component the control object so it knows where to store field's values, validate etc. we then bind it to what we render using field which contains those props and spread it inside the component which wires it up. we then use fieldState - the status of the particular field like its error value to show errors.

--- frontend/src/features/posts/components/CreatePostForm.tsx
+++ frontend/src/features/posts/components/CreatePostForm.tsx
@@ -1,12 +1,26 @@
-import { Button, Paper, TextField, Box} from '@mui/material';
-import {useForm} from 'react-hook-form'
+import { Button, Paper, TextField, Box,} from '@mui/material';
+import {useForm, Controller} from 'react-hook-form'
 //import { useState } from 'react';
 let renderCount = 0;
 function CreatePostForm() {
     renderCount++
     console.log(renderCount);
     //const [title, setTitle] = useState("")
-    const {register, handleSubmit } = useForm()
+    type FormData = {
+        title: string
+        content: string
+        contentTwo: string
+    }
+    const {register, handleSubmit, control, formState: {errors} } = useForm<FormData>({
+        defaultValues: {
+            title: "",
+            content: "",
+            contentTwo: ""
+        } //this kinda behaves like a typescript type check 
+        //these names match those field names hence if we typo it shows an error so recommended to give initial values
+    })
+    console.log(errors);
+    
     return (
         <div>
             
@@ -16,21 +30,40 @@ function CreatePostForm() {
             >
             <Paper elevation={3} className='flex flex-col gap-4 p-4'>
             <TextField 
-            required
             label="Post Title"
             className=''
             //onChange={e => setTitle(e.target.value)}
-            {...register("title", {required: true, minLength: 10})} //register can take a option object in which we can give validation rules like required and min length
+            {...register("title", {required: "This is required", minLength: {value: 5, message: "Minimum length is 5"}})} //register can take a option object in which we can give validation rules like required and min length
             //but theres no feedback if it fails some validation
             />
             <TextField 
-            required
             label="Post Content"
             multiline
             rows={6}
             className='w-full'
-            {...register("content", {required: true, minLength: 15})} //Each field is required to have a name as a key for the registration process.
+            error={errors.content? true :false }
+            helperText={errors.content?.message}
+            {...register("content", {required: "This is required", minLength: {value: 5, message: "Minimum length is 5"}})} //Each field is required to have a name as a key for the registration process.
+             />
+
+            <Controller 
+                name="contentTwo"
+                control={control}
+                rules={{required: "This is required",minLength: {value: 5, message: "Minimum length is 5"}}}
+                render={({field, fieldState}) => (
+                    <TextField 
+                        label="Post Content"
+                        multiline
+                        rows={6}
+                        {...field}
+                        className='w-full'
+                        error={!!fieldState.error} //double negation suppose no error itll conver undefined to false
+                        helperText={fieldState.error?.message}
+
+                    />
+                )}
             />
+            
             <Button type="submit" className='self-end' variant='contained'>Save</Button>
             </Paper>
 
