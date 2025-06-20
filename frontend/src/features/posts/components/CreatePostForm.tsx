ca4aca6fd05d6a54f1bba7bf5295d3b7deac6c7e
AfazPaul1 <afazpaul@gmail.com>
2025-06-20 18:22:37 +0530

    feat: react hook form integration body: - import the hook and destructure register and handlesubmit function - call the register function with a name which will be the key for that data - this registers the component into the hook making its dta available - we can also pass in options object with validations to register - we pass our event handler to handleSubmit function which validates the inputs and calls our eventhandler.

--- frontend/src/features/posts/components/CreatePostForm.tsx
+++ frontend/src/features/posts/components/CreatePostForm.tsx
@@ -1,15 +1,18 @@
 import { Button, Paper, TextField, Box} from '@mui/material';
+import {useForm} from 'react-hook-form'
 //import { useState } from 'react';
 let renderCount = 0;
 function CreatePostForm() {
     renderCount++
     console.log(renderCount);
     //const [title, setTitle] = useState("")
-    
+    const {register, handleSubmit } = useForm()
     return (
-        <div>     
+        <div>
+            
             <Box className='w-full sm:max-w-xl mx-auto my-2'
                 component="form"
+                onSubmit={handleSubmit(data => {console.log(data)})} //{title: 'a', content: 'a'}
             >
             <Paper elevation={3} className='flex flex-col gap-4 p-4'>
             <TextField 
@@ -17,6 +20,8 @@ function CreatePostForm() {
             label="Post Title"
             className=''
             //onChange={e => setTitle(e.target.value)}
+            {...register("title", {required: true, minLength: 10})} //register can take a option object in which we can give validation rules like required and min length
+            //but theres no feedback if it fails some validation
             />
             <TextField 
             required
@@ -24,10 +29,13 @@ function CreatePostForm() {
             multiline
             rows={6}
             className='w-full'
+            {...register("content", {required: true, minLength: 15})} //Each field is required to have a name as a key for the registration process.
             />
             <Button type="submit" className='self-end' variant='contained'>Save</Button>
             </Paper>
-            </Box>  
+
+            </Box>
+            
         </div>
     )
 }
