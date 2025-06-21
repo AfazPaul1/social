388c3a2df62f1785b281c903cfeee73648b343c8
AfazPaul1 <afazpaul@gmail.com>
2025-06-21 15:51:19 +0530

    refactor(React Hook Form): Use FormState's isValid property
    - Formstate contain info about the entire form state. from it we destructure the isValid property which is set to true if the form doesnt have any errors.
    - When the user submits the form validation is done. we can change that behaviour using mode useForm option which allows us to configure the validation strategy. setting mode to "onChange" triggers validation on every change event. so there is a possibility of multiple rerenders which could impact performance.
    - We want the button to be disabled untill all errors are cleared. For that we could use this isValid property with onChange to continously validate and use that validation boolean to disable button untill is turns true
    - Our previous approach was using a useWatch hook to watch for both fields and validate and disable. It used some duplication logic where we had a validation condition on the field and again a condition in the button. Suppose we were to change the field validation we would also have to change our disabling feature. So far havent noticed any major performance changes.  Also removed  a ton of code
    
    Issue #4

--- frontend/src/features/posts/components/CreatePostForm.tsx
+++ frontend/src/features/posts/components/CreatePostForm.tsx
@@ -10,23 +10,15 @@ export type FormData = {
         title: string
         content: string
     }
-//let renderCount = 0;
 function CreatePostForm() {
     const dispatch = useAppDispatch()
-    //renderCount++
-    //console.log(renderCount);
-    const {register, handleSubmit, control, formState: {errors} } = useForm<FormData>({
+    const {register, handleSubmit, control, formState: {errors, isValid} } = useForm<FormData>({
+        mode: "onChange", 
         defaultValues: {
             title: "",
             content: "",
         }
     })
-    //const title = watch("title")
-    //const formState = useFormState({control})
-    //console.log(formState);
-    
-    
-    
     return (
         <div>
             
@@ -37,10 +29,9 @@ function CreatePostForm() {
             <Paper elevation={3} className='flex flex-col gap-4 p-4'>
             <TextField 
             label="Post Title"
-            sx={{'& .MuiFormHelperText-root': {textAlign: 'right'}}} //& is a selector ref "Inside this TextField, find the element with class .MuiFormHelperText-root and apply textAlign: 'right'."
-            {...register("title", {required: "This is required", minLength: {value: 5, message: "Minimum length is 5"}})} //register can take a option object in which we can give validation rules like required and min length
+            sx={{'& .MuiFormHelperText-root': {textAlign: 'right'}}}
+            {...register("title", {required: "This is required", minLength: {value: 5, message: "Minimum length is 5"}})}
             error={!!errors.title}
-            //helperText={errors.title? errors.title.message :`${title.length} /300`}//watch inside same component is unnecessary? wait its not? chatgpt said no other way we can get it only way is to convert into using a controller
             helperText={errors.title? errors.title.message : <WordCount control={control}/>}
             />
             <Controller 
@@ -54,14 +45,12 @@ function CreatePostForm() {
                         rows={6}
                         {...field}
                         className='w-full'
-                        error={!!fieldState.error} //double negation suppose no error itll conver undefined to false
+                        error={!!fieldState.error} 
                         helperText={fieldState.error? fieldState.error.message : `${field.value.length}/1000 `}
                     />
                 )}
             />
-            
-            {/* <Button disabled={title.length < 5} type="submit" className='self-end' variant='contained'>Save</Button> */}
-            <SaveButton control={control} />
+            <SaveButton isValid = {isValid}/>
             </Paper>
 
             </Box>
--- frontend/src/features/posts/components/SaveButton.tsx
+++ frontend/src/features/posts/components/SaveButton.tsx
@@ -1,15 +1,7 @@
-import { useWatch } from "react-hook-form";
-import type { Control } from "react-hook-form";
-import type { FormData } from "./CreatePostForm";
 import { Button } from "@mui/material";
-export default function SaveButton({control}: {control: Control<FormData>}){
-    const useDisBut = ({control} : {control:Control<FormData>}) => {
-        const [content, title] =  useWatch({name:["content", "title"],  control})
-        return content.length >= 5 && title.length >= 5
-    }
-    console.log(useDisBut({control}));
+export default function SaveButton({ isValid}: { isValid:boolean}){
     
     return <>
-        <Button disabled={!useDisBut({control})}  type="submit" className='self-end' variant='contained'>Save</Button>       
+        <Button disabled={!isValid}  type="submit" className='self-end' variant='contained'>Save</Button>       
     </>
 }
